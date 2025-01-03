6c3195e3af8210f6fec081aaa8491d85062a5354
neo <anthony.wentzel@gmail.com>
2025-01-03 10:11:54 -0500

    fix(vercel): debugging handler

--- vercel/install.sh
+++ vercel/install.sh
@@ -8,8 +8,7 @@ rm -rf node_modules
 # 2. Only install production node_modules
 yarn workspaces focus --production
 
+# 3. Remove and prune uncessary files inside node_modules directory
 # du -sh ./node_modules/* | sort -nr | grep '\dM.*'
 # npx modclean --patterns="default:safe,default:caution" --additional-patterns="*.xls?(x),*.ppt?(x),*.rtf" --ignore="validate-npm-license,readme*"
 npx clean-modules clean '**/*.d.ts' '**/@types/**' '!**/*.png' '!**/*.xls' '!**/*.ppt' '!**/*.rtf' -y
-
-du -sh ./node_modules/* | sort -nr | grep '\dM.*'
