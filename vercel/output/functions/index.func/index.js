1ae0b77629b2de0beecd48b727d40663e29f0baa
neo <anthony.wentzel@gmail.com>
2025-01-03 12:30:42 -0500

    fix(api): cleanup parameter store with large strings

--- vercel/output/functions/index.func/index.js
+++ vercel/output/functions/index.func/index.js
@@ -1,4 +1,11 @@
-import './polyfill-require-in-esm.mjs'
+// import './polyfill-require-in-esm.mjs'
+
+import { createRequire } from 'node:module';
+import url from 'node:url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+globalThis.require = createRequire(__filename);
+
 import { handler as oneHandler } from '../../../../dist/index.js'
 
 export default async function handler(req, res) {
