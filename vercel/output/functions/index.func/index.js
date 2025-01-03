af7594d8cf1e5a3404f388fe100e99a48983f02b
neo <anthony.wentzel@gmail.com>
2025-01-03 08:50:43 -0500

    fix(vercel): debugging handler

--- vercel/output/functions/index.func/index.js
+++ vercel/output/functions/index.func/index.js
@@ -1,9 +1,10 @@
-import { handler as oneHandler } from '../../../../dist/index.js'
+// import { handler as oneHandler } from '../../../../dist/index.js'
 
 export default async function handler(req, res) {
   const { url } = req
 
   console.log('Request to url:', url)
 
-  return await oneHandler(req, res)
+  // return await oneHandler(req, res)
+  return { status: 200 }
 }
